fd6af1bbc962e947c4ba10a441636f17beb4c3bb
M4th3wss <sabamiqeladze43@gmail.com>
2026-09-07 23:16:52 +0400

    final commit before abandoning this

--- app.js
+++ app.js
@@ -32,65 +32,65 @@ app.post('/add', mainController.createAnimePost);
 app.delete('/delete/:name', mainController.deleteAnime);
 
 
-//Sign up
-app.get("/sign-up", mainController.signUpGet);
-app.post("/sign-up", mainController.signUpPost);
-//Log in
-app.post(
-  "/log-in",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/",
-    failureMessage: true,
-  })
-);
+//Was planning to add sign up but im too lazy for this
+// app.get("/sign-up", mainController.signUpGet);
+// app.post("/sign-up", mainController.signUpPost);
+// //Log in
+// app.post(
+//   "/log-in",
+//   passport.authenticate("local", {
+//     successRedirect: "/",
+//     failureRedirect: "/",
+//     failureMessage: true,
+//   })
+// );
 
 
 
 
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
-      const user = rows[0];
+// passport.use(
+//   new LocalStrategy(async (username, password, done) => {
+//     try {
+//       const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+//       const user = rows[0];
 
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      if (user.password !== password) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
-    } catch(err) {
-      return done(err);
-    }
-  })
-);
+//       if (!user) {
+//         return done(null, false, { message: "Incorrect username" });
+//       }
+//       if (user.password !== password) {
+//         return done(null, false, { message: "Incorrect password" });
+//       }
+//       return done(null, user);
+//     } catch(err) {
+//       return done(err);
+//     }
+//   })
+// );
 
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// });
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
-    const user = rows[0];
+// passport.deserializeUser(async (id, done) => {
+//   try {
+//     const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+//     const user = rows[0];
 
-    done(null, user);
-  } catch(err) {
-    done(err);
-  }
-});
+//     done(null, user);
+//   } catch(err) {
+//     done(err);
+//   }
+// });
 
-app.post(
-  "/log-in",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/",
-    failureMessage: true,
-  })
-);
+// app.post(
+//   "/log-in",
+//   passport.authenticate("local", {
+//     successRedirect: "/",
+//     failureRedirect: "/",
+//     failureMessage: true,
+//   })
+// );
 
 
 
